a950f3ea3ec014971a431178076df1762d5fb932
Per Sandström <pg.sandstrom@gmail.com>
2021-05-13 18:39:26 +0200

    Add some mentak stuff

---
+++ core/races/hacan.test.ts
@@ -0,0 +1,45 @@
+import getBattleReport from '..'
+import { checkResult } from '../../util/util.test'
+import { Participant } from '../battle-types'
+import { getUnitMap } from '../battleSetup'
+import { Race, Place } from '../enums'
+import { DO_BATTLE_X_TIMES } from '../index.test'
+
+describe('Hacan', () => {
+  it('Hacan flagship should give different results with different numbers of trade goods bonuses', () => {
+    const attacker: Participant = {
+      race: Race.hacan,
+      units: getUnitMap(),
+      unitUpgrades: {},
+      riskDirectHit: false,
+      side: 'attacker',
+      battleEffects: { 'Hacan flagship trade goods': 1 },
+    }
+    const defender: Participant = {
+      race: Race.barony_of_letnev,
+      units: getUnitMap(),
+      unitUpgrades: {},
+      riskDirectHit: false,
+      side: 'defender',
+      battleEffects: {},
+    }
+    attacker.units.flagship = 1
+    attacker.units.cruiser = 5
+    defender.units.flagship = 1
+    defender.units.cruiser = 5
+
+    const result = getBattleReport(attacker, defender, Place.space, DO_BATTLE_X_TIMES)
+
+    checkResult(result.attacker, DO_BATTLE_X_TIMES * 0.386)
+    checkResult(result.draw, DO_BATTLE_X_TIMES * 0.106, 0.1)
+    checkResult(result.defender, DO_BATTLE_X_TIMES * 0.507)
+
+    attacker.battleEffects['Hacan flagship trade goods'] = 100
+
+    const result2 = getBattleReport(attacker, defender, Place.space, DO_BATTLE_X_TIMES)
+
+    checkResult(result2.attacker, DO_BATTLE_X_TIMES * 0.469)
+    checkResult(result2.draw, DO_BATTLE_X_TIMES * 0.117, 0.1)
+    checkResult(result2.defender, DO_BATTLE_X_TIMES * 0.413)
+  })
+})
--- core/races/mentak.ts
+++ core/races/mentak.ts
@@ -1,4 +1,6 @@
+import { ParticipantInstance, BattleInstance } from '../battle-types'
 import { BattleEffect } from '../battleeffect/battleEffects'
+import { getHits } from '../roll'
 import { defaultRoll, UnitInstance, UnitType } from '../unit'
 
 export const mentak: BattleEffect[] = [
@@ -14,14 +16,85 @@ export const mentak: BattleEffect[] = [
             hit: 7,
             count: 2,
           },
+          aura: [
+            {
+              name: 'Mentak flagship ability',
+              type: 'other',
+              transformEnemyUnit: (u: UnitInstance) => {
+                // TODO this shouldnt work on a unit that is both ship and ground force
+                // maybe that will mess with the nomad mech?
+                // TODO test this ship with assault cannon. It should snipe the ship and retain sustain damage
+                if (u.isShip) {
+                  return {
+                    ...u,
+                    sustainDamage: false,
+                  }
+                } else {
+                  return u
+                }
+              },
+            },
+          ],
         }
       } else {
         return unit
       }
     },
   },
-  // TODO add flagship ability
-  // TODO add mech
-  // TODO add ambush
+  {
+    name: 'Mentak mech',
+    type: 'race',
+    transformUnit: (u: UnitInstance) => {
+      if (u.type === UnitType.mech) {
+        return {
+          ...u,
+          aura: [
+            {
+              name: 'Mentak mech ability',
+              type: 'other',
+              transformEnemyUnit: (u: UnitInstance) => {
+                if (u.isGroundForce) {
+                  return {
+                    ...u,
+                    sustainDamage: false,
+                  }
+                } else {
+                  return u
+                }
+              },
+            },
+          ],
+        }
+      } else {
+        return u
+      }
+    },
+  },
+  {
+    name: 'Ambush',
+    type: 'race',
+    onStart: (
+      participant: ParticipantInstance,
+      _battle: BattleInstance,
+      otherParticipant: ParticipantInstance,
+    ) => {
+      const cruisers = participant.units.filter((u) => u.type === UnitType.cruiser).slice(0, 2)
+      const destroyers = participant.units
+        .filter((u) => u.type === UnitType.destroyer)
+        .slice(0, 2 - cruisers.length)
+
+      const ambushShips = [...cruisers, ...destroyers]
+
+      let hits = 0
+      for (const ambushShip of ambushShips) {
+        const hit = getHits({
+          ...defaultRoll,
+          hit: ambushShip.combat!.hit,
+        })
+        hits += hit.hits
+      }
+      otherParticipant.hitsToAssign.hits += hits
+    },
+  },
   // TODO maybe add hero???
 ]
