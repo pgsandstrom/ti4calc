33be30d658fe3fe6469fb2d910e0663a2a347170
Per Sandström <pg.sandstrom@gmail.com>
2021-06-02 10:54:29 +0200

    Fix broken test

--- core/unitGet.test.ts
+++ core/unitGet.test.ts
@@ -1,8 +1,8 @@
 import { Battle, Participant } from './battle-types'
-import { getUnitMap, setupBattle } from './battleSetup'
+import { createParticipant, getUnitMap, setupBattle } from './battleSetup'
 import { Race, Place } from './enums'
 import { UnitType } from './unit'
-import { getBestShip } from './unitGet'
+import { getBestShip, isHighestHitUnit } from './unitGet'
 
 describe('unitGet', () => {
   it('getBestShip', () => {
@@ -40,5 +40,50 @@ describe('unitGet', () => {
     const bestShip = getBestShip(participantInstance)
 
     expect(bestShip!.type).toEqual(UnitType.warsun)
+  })
+
+  it('getHighestHitUnit', () => {
+    const attacker = createParticipant('attacker')
+    attacker.race = Race.l1z1x
+    attacker.units.destroyer = 1
+    attacker.units.cruiser = 1
+    const defender = createParticipant('defender')
+    const battle: Battle = {
+      attacker,
+      defender,
+      place: Place.space,
+    }
+    const battleInstance = setupBattle(battle)
+
+    const cruiser = battleInstance.attacker.units.find((u) => u.type === UnitType.cruiser)!
+    const isCruiserHighest = isHighestHitUnit(cruiser, battleInstance.attacker, 'combat')
+    expect(isCruiserHighest).toEqual(true)
+
+    const destroyer = battleInstance.attacker.units.find((u) => u.type === UnitType.destroyer)!
+    const isDestroyerHighest = isHighestHitUnit(destroyer, battleInstance.attacker, 'combat')
+    expect(isDestroyerHighest).toEqual(false)
+  })
+
+  it('isHighestHitUnit should work with unit upgrades', () => {
+    const attacker = createParticipant('attacker')
+    attacker.race = Race.l1z1x
+    attacker.units.flagship = 1
+    attacker.units.dreadnought = 1
+    attacker.unitUpgrades[UnitType.dreadnought] = true
+    const defender = createParticipant('defender')
+    const battle: Battle = {
+      attacker,
+      defender,
+      place: Place.space,
+    }
+    const battleInstance = setupBattle(battle)
+
+    const flagShip = battleInstance.attacker.units.find((u) => u.type === UnitType.flagship)!
+    const isFlagshipHighest = isHighestHitUnit(flagShip, battleInstance.attacker, 'combat')
+    expect(isFlagshipHighest).toEqual(false)
+
+    const dreadnought = battleInstance.attacker.units.find((u) => u.type === UnitType.dreadnought)!
+    const isDreadnoughtHighest = isHighestHitUnit(dreadnought, battleInstance.attacker, 'combat')
+    expect(isDreadnoughtHighest).toEqual(true)
   })
 })
