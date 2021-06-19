fa02b00c8e760cdc79f45ea2dea8aa4b1fcd56d7
Per Sandstrom <hello@persandstrom.com>
2021-06-19 13:15:29 +0200

    Change how we prevent raceimage ssr

--- component/raceImage.tsx
+++ component/raceImage.tsx
@@ -1,7 +1,7 @@
 // import Image from 'next/image'
+import { useLayoutEffect, useState } from 'react'
 import styled from 'styled-components'
 import { Race } from '../core/enums'
-import dynamic from 'next/dynamic'
 
 const StyledDiv = styled.div<Props>`
   @media (max-width: 1023px) {
@@ -21,13 +21,18 @@ interface Props {
   style?: React.CSSProperties
 }
 
-// We avoid ssr for this image since it might change as soon as we find a saved race in localStorage.
-// Having a big image flash quickly before it is replaced is a big ugly. Better to have the images be slower to load.
-const DynamicComponentWithNoSSR = dynamic(() => import('./raceImage2'), { ssr: false })
-
 export default function RaceImage(props: Props) {
   // Do not use next/image before they cache it properly
   // In the meantime I just use it to generate optimized images in the correct size, then use that image in a normal <img />
+
+  const [url, setUrl] = useState<string>()
+
+  // the race is retrieved from localstorage. Showing the race before that would result in an ugly flash.
+  // So here we make sure no image is set in ssr.
+  useLayoutEffect(() => {
+    setUrl(`/races/small/${props.race.replaceAll(' ', '_').replaceAll("'", '').toLowerCase()}.webp`)
+  }, [props.race])
+
   return (
     <StyledDiv {...props}>
       {/* <Image
@@ -36,7 +41,13 @@ export default function RaceImage(props: Props) {
         width={640}
         height={828}
       /> */}
-      <DynamicComponentWithNoSSR {...props} />
+      <img
+        src={url}
+        alt=""
+        width={640}
+        height={828}
+        style={{ float: props.side === 'left' ? 'right' : 'left' }}
+      />
     </StyledDiv>
   )
 }
