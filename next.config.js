2cc6ca73c567cb0004e89fc276d8458a2e131bc5
Per Sandstrom <hello@persandstrom.com>
2021-11-17 20:40:46 +0100

    Fix config for nextjs 12

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  // there is some webpack plugin that does not work well currently. I think it was something with web worker.
-  webpack5: false,
   eslint: {
     ignoreDuringBuilds: true,
   },
