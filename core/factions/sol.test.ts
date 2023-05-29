f76b8cdc4dadf2a4989371fc94f03e3d3fefd8be
Trevor Kropp <kropptrevor@gmail.com>
2023-05-29 00:01:29 -0500

    Add Sol faction upgraded infantry test

--- core/factions/sol.test.ts
+++ core/factions/sol.test.ts
@@ -1,4 +1,4 @@
-import getBattleReport from '..'
+import getBattleReport, { BattleReport } from '..'
 import { checkResult, getTestParticipant } from '../../util/util.test'
 import { Faction, Place } from '../enums'
 import { DO_BATTLE_X_TIMES } from '../index.test'
@@ -23,15 +23,57 @@ describe('Sol', () => {
 
     const result = getBattleReport(attacker, defender, Place.ground, DO_BATTLE_X_TIMES)
 
-    // using closed form for geometric series
-    // P(Sol hitting, Muaat missing) / (1 - P(both missing))
-    const attackerWinChance = (0.7 * 0.4) / (1 - 0.7 * 0.6)
-    checkResult(result.attacker, DO_BATTLE_X_TIMES * attackerWinChance)
-    // P(both hitting) / (1 - P(both missing))
-    const drawChance = (0.3 * 0.4) / (1 - 0.7 * 0.6)
-    checkResult(result.draw, DO_BATTLE_X_TIMES * drawChance)
-    // P(Sol missing, Sol hitting) / (1 - P(both missing))
-    const defenderWinChance = (0.3 * 0.6) / (1 - 0.7 * 0.6)
-    checkResult(result.defender, DO_BATTLE_X_TIMES * defenderWinChance)
+    check1v1InfantryResult(result, 0.4, 0.3)
+  })
+
+  it('should have stronger upgraded infantry', () => {
+    const attacker = getTestParticipant(
+      'attacker',
+      {
+        infantry: 1,
+      },
+      Faction.sol,
+      {},
+      {
+        infantry: true,
+      },
+    )
+
+    const defender = getTestParticipant(
+      'defender',
+      {
+        infantry: 1,
+      },
+      Faction.muaat,
+      {},
+      {
+        infantry: true,
+      },
+    )
+
+    const result = getBattleReport(attacker, defender, Place.ground, DO_BATTLE_X_TIMES)
+
+    check1v1InfantryResult(result, 0.5, 0.4)
   })
 })
+
+function check1v1InfantryResult(
+  result: BattleReport,
+  attackerHitChance: number,
+  defenderHitChance: number,
+) {
+  const aHitChance = attackerHitChance
+  const aMissChance = 1 - aHitChance
+  const dHitChance = defenderHitChance
+  const dMissChance = 1 - dHitChance
+  // using closed form for geometric series
+  // P(Muaat missing and Sol hitting) / (1 - P(both missing))
+  const attackerWinChance = (dMissChance * aHitChance) / (1 - dMissChance * aMissChance)
+  checkResult(result.attacker, DO_BATTLE_X_TIMES * attackerWinChance)
+  // P(both hitting) / (1 - P(both missing))
+  const drawChance = (dHitChance * aHitChance) / (1 - dMissChance * aMissChance)
+  checkResult(result.draw, DO_BATTLE_X_TIMES * drawChance)
+  // P(Muaat hitting and Sol missing) / (1 - P(both missing))
+  const defenderWinChance = (dHitChance * aMissChance) / (1 - dMissChance * aMissChance)
+  checkResult(result.defender, DO_BATTLE_X_TIMES * defenderWinChance)
+}
