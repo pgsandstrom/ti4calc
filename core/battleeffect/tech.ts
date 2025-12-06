a27068eee8cf8166e5d485871179783fb04fe6e5
Ben Levin <bplevin36@gmail.com>
2025-12-06 14:01:31 +0000

    move experimental battlestation to beforeStart

--- core/battleeffect/tech.ts
+++ core/battleeffect/tech.ts
@@ -42,7 +42,7 @@ export const plasmaScoring: BattleEffect = {
 export const magenDefenseGrid: BattleEffect = {
   name: 'Magen Defense Grid',
   description:
-    "When any player activates a system that contains 1 or more of your structures, place 1 infantry from your reinforcements with each of those structures. At the start of ground combat on a planet that contains 1 or more of your structures, produce 1 hit and assign it to 1 of your opponent's ground forces.\nPLEASE NOTE: We dont place extra infantry, increase the count yourself. Checking this assumes you have at least one building.",
+    "When any player activates a system that contains 1 or more of your structures, place 1 infantry from your reinforcements with each of those structures. At the start of ground combat on a planet that contains 1 or more of your structures, produce 1 hit and assign it to 1 of your opponent's ground forces.\nPLEASE NOTE: We dont place extra infantry, increase the count yourself. Checking this assumes you have at least one structure.",
   type: 'tech',
   place: Place.ground,
   side: 'defender',
